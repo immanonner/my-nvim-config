e04272fd10cda471ab0980d8e9e4adb10fb630c0
LinuxNonner <ssw.williams@gmail.com>
2024-03-21 23:35:58 +0900

    add some initial lsp configuration

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,19 +1,16 @@
 return {
-    {"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-    config = function () 
-      local configs = require("nvim-treesitter.configs")
-
-      configs.setup({
-          ensure_installed = { "lua", "javascript", "html", "python", "css", "json",
-                "bash",  },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },  
-        })
-    end
-
-
-
-	},
-}	
+            {"nvim-treesitter/nvim-treesitter",
+	         	build = ":TSUpdate",
+                config = function () 
+                    local configs = require("nvim-treesitter.configs")
+                    configs.setup({
+                    ensure_installed = { "lua", "javascript", "html", "python", "css", "json",
+                     "bash",  },
+                    sync_install = false,
+                    highlight = { enable = true },
+                    indent = { enable = true },  
+                    rainbow = { enable = true },
+                                 })
+                end}
+        }
+        	
