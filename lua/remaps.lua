f2145030996aca28f7f3c1224d03e442af512647
LinuxNonner <ssw.williams@gmail.com>
2024-04-03 14:44:31 +0200

    include telekasten remaps; move copilot workspace folders

--- lua/remaps.lua
+++ lua/remaps.lua
@@ -19,5 +19,22 @@ vim.api.nvim_set_keymap("n", "<leader>b", ":bprev<CR>", { noremap = true, silent
 vim.api.nvim_set_keymap("n", "<leader>f", ":bnext<CR>", { noremap = true, silent = true })
 
 -------Plugin Remaps-------
--- remap leader key top space
-vim.g.copilot_workspace_folders = { "~/.config/nvim/", "~/ScriptingPlayground/" }
+
+-- Telekasten
+-- Launch panel if nothing is typed after <leader>z
+vim.keymap.set("n", "<leader>z", "<cmd>Telekasten panel<CR>")
+
+-- Most used functions
+vim.keymap.set("n", "<leader>zf", "<cmd>Telekasten find_notes<CR>")
+vim.keymap.set("n", "<leader>zg", "<cmd>Telekasten search_notes<CR>")
+vim.keymap.set("n", "<leader>zd", "<cmd>Telekasten goto_today<CR>")
+vim.keymap.set("n", "<leader>zz", "<cmd>Telekasten follow_link<CR>")
+vim.keymap.set("n", "<leader>zn", "<cmd>Telekasten new_note<CR>")
+vim.keymap.set("n", "<leader>zc", "<cmd>Telekasten show_calendar<CR>")
+vim.keymap.set("n", "<leader>zb", "<cmd>Telekasten show_backlinks<CR>")
+vim.keymap.set("n", "<leader>zI", "<cmd>Telekasten insert_img_link<CR>")
+
+-- Call insert link automatically when we start typing a link
+vim.keymap.set("i", "[[", "<cmd>Telekasten insert_link<CR>")
+
+
